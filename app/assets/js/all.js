83e4f7c8262b7c343f6cdb85eb474247e8a23f0f
potatolee <dean19990226@gmail.com>
2022-11-25 00:01:43 +0800

    短衝規劃講解，產品代辦清單判斷尚未完全

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,6 @@
 const candidateDOM = document.querySelector(".candidate");
 const productBacklogDOM = document.querySelector(".productBacklog");
+const btnOk = document.querySelector(".btnOk");
 const isArrEqual = (arr1, arr2) => {
   return arr1.join("") === arr2.join("") ? true : false;
 };
@@ -9,6 +10,7 @@ var candidate = Sortable.create(candidateDOM, {
   group: "shart",
   animation: 500
 });
+
 
 var productBacklog = Sortable.create(productBacklogDOM, {
   group: "shart",
@@ -20,9 +22,56 @@ var productBacklog = Sortable.create(productBacklogDOM, {
     if (answerrr === true) {
       answerMessage.innerHTML =
         "<p class='messageCorrect'>" + "順序正確" + "</p>";
+        // 答對彈跳視窗 start
+        const modalTestOk = document.querySelector(".modal__test--ok");
+        addClass(btnOk,"bg-secondary")
+        btnOk.addEventListener("click",function(e){
+        e.preventDefault();
+        addClass(modalTestOk,"open"); 
+      })
+    // 答對彈跳視窗 end
     } else {
       answerMessage.innerHTML =
-        "<p class='messageWorng'>" + "順序錯誤" + "</p>";
+        "<p class='messageWrong'>" + "順序錯誤" + "</p>";
+        // 答錯彈跳視窗 start
+        // const modalTestWrong = document.querySelector(".modal__test--wrong");
+        // btnOk.addEventListener("click",function(e){
+        // e.preventDefault();
+        //  addClass(modalTestWrong,"open");
+        //  console.log("答錯了");
+        // })
+      // 答錯彈跳視窗 end
+
     }
   }
 });
+
+function hasClass(el, className) {
+  if (el.classList)
+      return el.classList.contains(className);
+  else
+      return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+}
+    
+function addClass(el, className) {
+  if (el.classList)
+      el.classList.add(className);
+  else if (!hasClass(el, className)) {
+      el.className += " " + className;
+  }
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+      el.classList.remove(className);
+  else if (hasClass(el, className)) {
+      var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+      el.className=el.className.replace(reg, ' ');
+  }
+}
+
+
+
+
+
+
