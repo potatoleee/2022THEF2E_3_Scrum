15e3fe66ea23c93d976231c839428f00ceb45851
potatolee <dean19990226@gmail.com>
2022-11-27 14:16:51 +0800

    check

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,50 +1,56 @@
-const candidateDOM = document.querySelector(".candidate");
-const productBacklogDOM = document.querySelector(".productBacklog");
-const btnOk = document.querySelector(".btnOk");
-const isArrEqual = (arr1, arr2) => {
-  return arr1.join("") === arr2.join("") ? true : false;
-};
-const answerAry = ["1", "2", "3", "4"];
+//代辦清單start
+const dragGame = () =>{
 
-var candidate = Sortable.create(candidateDOM, { 
-  group: "shart",
-  animation: 500
-});
+  const candidateDOM = document.querySelector(".candidate");
+  const productBacklogDOM = document.querySelector(".productBacklog");
+  const btnOk = document.querySelector(".btnOk");
+  const isArrEqual = (arr1, arr2) => {
+    return arr1.join("") === arr2.join("") ? true : false;
+  };
+  const answerAry = ["1", "2", "3", "4"];
+
+  var candidate = Sortable.create(candidateDOM, { 
+    group: "shart",
+    animation: 500
+  });
 
 
-var productBacklog = Sortable.create(productBacklogDOM, {
-  group: "shart",
-  onSort: () => {
-    const answerMessage = document.querySelector(".message");
-    let order = productBacklog.toArray();
-    console.log(order);
-    const answerrr = isArrEqual(order, answerAry); // true
-    if (answerrr === true) {
-      answerMessage.innerHTML =
-        "<p class='messageCorrect'>" + "順序正確" + "</p>";
-        // 答對彈跳視窗 start
-        const modalTestOk = document.querySelector(".modal__test--ok");
-        addClass(btnOk,"bg-secondary")
-        btnOk.addEventListener("click",function(e){
-        e.preventDefault();
-        addClass(modalTestOk,"open"); 
-      })
-    // 答對彈跳視窗 end
-    } else {
-      answerMessage.innerHTML =
-        "<p class='messageWrong'>" + "順序錯誤" + "</p>";
-        // 答錯彈跳視窗 start
-        // const modalTestWrong = document.querySelector(".modal__test--wrong");
-        // btnOk.addEventListener("click",function(e){
-        // e.preventDefault();
-        //  addClass(modalTestWrong,"open");
-        //  console.log("答錯了");
-        // })
-      // 答錯彈跳視窗 end
+  var productBacklog = Sortable.create(productBacklogDOM, {
+    group: "shart",
+    onSort: () => {
+      const answerMessage = document.querySelector(".message");
+      let order = productBacklog.toArray();
+      console.log(order);
+      const answerrr = isArrEqual(order, answerAry); // true
+      if (answerrr === true) {
+        answerMessage.innerHTML =
+          "<p class='messageCorrect'>" + "順序正確" + "</p>";
+          // 答對彈跳視窗 start
+          const modalTestOk = document.querySelector(".modal__test--ok");
+          addClass(btnOk,"bg-secondary")
+          btnOk.addEventListener("click",function(e){
+          e.preventDefault();
+          addClass(modalTestOk,"open"); 
+        })
+      // 答對彈跳視窗 end
+      } else {
+        answerMessage.innerHTML =
+          "<p class='messageWrong'>" + "順序錯誤" + "</p>";
+          // 答錯彈跳視窗 start
+          const modalTestWrong = document.querySelector(".modal__test--wrong");
+          btnOk.addEventListener("click",function(e){
+          e.preventDefault();
+          addClass(modalTestWrong,"open");
+          console.log("答錯了");
+          })
+        // 答錯彈跳視窗 end
 
+      }
     }
-  }
-});
+  });
+
+}//代辦清單 end
+
 
 function hasClass(el, className) {
   if (el.classList)
@@ -70,8 +76,9 @@ function removeClass(el, className) {
   }
 }
 
+//敏捷分數計算 start
+const sprintGame = () =>{
 
-//敏捷分數計算
 const backlogDOM = document.querySelector(".backlog-section .droppable-container");
 console.log(backlogDOM);
 const sprintDOM = document.querySelector(".sprint-section .droppable-container");
@@ -149,7 +156,13 @@ let backlogSortableObj = Sortable.create(backlogDOM, {
   }
 });
 
-//敏捷分數計算
+}//敏捷分數計算 end
+
+
+dragGame()
+sprintGame()
+
+
 
 
 
