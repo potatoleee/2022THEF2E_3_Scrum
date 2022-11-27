3babdb01cab006acc729857e1a7c808f17385a4e
potatolee <dean19990226@gmail.com>
2022-11-27 13:13:00 +0800

    計算分數列表出不來

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -71,13 +71,12 @@ function removeClass(el, className) {
 }
 
 
+//敏捷分數計算
+const backlogDOM = document.querySelector(".backlog-section .droppable-container");
+console.log(backlogDOM);
+const sprintDOM = document.querySelector(".sprint-section .droppable-container");
+console.log(sprintDOM);
 
-const backlogDOM = document.querySelector(
-  ".backlog-section .droppable-container"
-);
-const sprintDOM = document.querySelector(
-  ".sprint-section .droppable-container"
-);
 
 const backlogArr = [
   { content: "前台職缺列表（職缺詳細內容、點選可發送應徵意願）", score: 5 },
@@ -90,7 +89,7 @@ let totalScore = 0;
 let totalScoreDOM = document.querySelector(".total-score");
 totalScoreDOM.textContent = totalScore;
 
-// 以map方式創造DOM node, 並塞入backlog的container
+// // 以map方式創造DOM node, 並塞入backlog的container
 backlogArr.map((ele) => {
   let draggableCard = document.createElement("div");
   draggableCard.setAttribute("data-score", ele.score);
@@ -107,12 +106,50 @@ backlogArr.map((ele) => {
   backlogDOM.appendChild(draggableCard);
 });
 
+let sprintSortableObj = Sortable.create(sprintDOM, {
+  group: "dnd",
+  animation: 10,
+  dataIdAttr: "data-score",
 
+  onEnd: (event) => {
+    // 更新t// 更新totalSccore
+    totalScore = sprintSortableObj
+      .toArray()
+      .map((ele) => parseInt(ele, 10))
+      .reduce((a, b) => a + b, 0);
+    totalScoreDOM.textContent = totalScore;
 
+    let warningTextDOM = document.querySelector(".warning-text");
 
+    warningTextDOM.classList.add("hidden");
+    if (totalScore > 20) {
+      warningTextDOM.classList.remove("hidden");
+    }
+  }
+});
 
+let backlogSortableObj = Sortable.create(backlogDOM, {
+  group: "dnd",
+  animation: 10,
+  dataIdAttr: "data-score",
 
+  onEnd: (event) => {
+    totalScore = sprintSortableObj
+      .toArray()
+      .map((ele) => parseInt(ele, 10))
+      .reduce((a, b) => a + b, 0);
+    totalScoreDOM.textContent = totalScore;
 
+    let warningTextDOM = document.querySelector(".warning-text");
+
+    warningTextDOM.classList.add("hidden");
+    if (totalScore > 20) {
+      warningTextDOM.classList.remove("hidden");
+    }
+  }
+});
+
+//敏捷分數計算
 
 
 
