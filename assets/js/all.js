ec7b8fe575d0c8f7e1a440872c3a9a2c562d4ab4
potatolee <dean19990226@gmail.com>
2022-11-28 11:53:23 +0800

    Update 2022-11-28T03:53:21.812Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,9 @@
 "use strict";
 
-//代辦清單start
+var candidateDOM = document.querySelector(".candidate");
+var backlogDOM = document.querySelector(".backlog-section .droppable-container"); //代辦清單start
+
 var dragGame = function dragGame() {
-  var candidateDOM = document.querySelector(".candidate");
   var productBacklogDOM = document.querySelector(".productBacklog");
   var btnOk = document.querySelector(".btnOk");
 
@@ -37,8 +38,8 @@ var dragGame = function dragGame() {
 
         var modalTestWrong = document.querySelector(".modal__test--wrong");
         btnOk.addEventListener("click", function (e) {
-          e.preventDefault();
-          addClass(modalTestWrong, "open");
+          e.preventDefault(); // addClass(modalTestWrong,"open");
+
           console.log("答錯了");
         }); // 答錯彈跳視窗 end
       }
@@ -66,22 +67,24 @@ function removeClass(el, className) {
 
 
 var sprintGame = function sprintGame() {
-  var backlogDOM = document.querySelector(".backlog-section .droppable-container");
   console.log(backlogDOM);
   var sprintDOM = document.querySelector(".sprint-section .droppable-container");
   console.log(sprintDOM);
   var backlogArr = [{
-    content: "前台職缺列表（職缺詳細內容、點選可發送應徵意願）",
-    score: 5
+    title: "前台職缺列表",
+    content: "職缺詳細內容、點選可發送應徵意願",
+    score: "5"
   }, {
-    content: "應徵者的線上履歷編輯器",
-    score: 13
+    title: "應徵者的線上履歷編輯器",
+    score: "13"
   }, {
-    content: "會員系統（登入、註冊、權限管理）",
-    score: 8
+    title: "會員系統",
+    content: "登入、註冊、權限管理",
+    score: "8"
   }, {
-    content: "後台職缺管理功能（資訊上架、下架、顯示應徵者資料）",
-    score: 8
+    title: "後台職缺管理功能",
+    content: "資訊上架、下架、顯示應徵者資料",
+    score: "8"
   }];
   var totalScore = 0;
   var totalScoreDOM = document.querySelector(".total-score");
@@ -89,15 +92,25 @@ var sprintGame = function sprintGame() {
 
   backlogArr.map(function (ele) {
     var draggableCard = document.createElement("div");
+    var draggableText = document.createElement("p");
     draggableCard.setAttribute("data-score", ele.score);
     draggableCard.setAttribute("draggable", "true");
     draggableCard.classList.add("draggble");
-    draggableCard.textContent = ele.content;
-    var timeAvatar = document.createElement("div");
+    draggableCard.textContent = ele.title;
+    draggableCard.appendChild(draggableText);
+    draggableText.textContent = ele.content;
+    draggableText.classList.add("fs-8", "my-2");
+    var timeAvatar = document.createElement("ul");
     timeAvatar.classList.add("time-avatar");
-    timeAvatar.textContent = ele.score;
+    timeAvatar.textContent = ele.score + ' 點'; // timeAvatar.textContent = ele.score;
+
     draggableCard.appendChild(timeAvatar);
-    backlogDOM.appendChild(draggableCard);
+    backlogDOM.appendChild(draggableCard); //產出對應數量li
+
+    for (var i = 0; i < ele.score; i++) {
+      var boxes = document.createElement("li");
+      timeAvatar.appendChild(boxes);
+    }
   });
   var sprintSortableObj = Sortable.create(sprintDOM, {
     group: "dnd",
@@ -141,6 +154,11 @@ var sprintGame = function sprintGame() {
 }; //敏捷分數計算 end
 
 
-dragGame();
-sprintGame();
+if (candidateDOM) {
+  dragGame();
+}
+
+if (backlogDOM) {
+  sprintGame();
+}
 //# sourceMappingURL=all.js.map
